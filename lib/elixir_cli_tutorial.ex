950cbde528a25bdfa3fde282b6150b4995a86d94
Steven Leiva <leiva.steven@gmail.com>
2016-04-08 19:55:40 -0400

    Create an example Exlir CLI Application

--- lib/elixir_cli_tutorial.ex
+++ lib/elixir_cli_tutorial.ex
@@ -1,2 +1,18 @@
 defmodule ElixirCliTutorial do
+  def main(args) do
+    args |> parse_args |> process
+  end
+
+  def process([]) do
+    IO.puts "No arguments given"
+  end
+
+  def process([{:out, output}]) do
+    IO.puts output
+  end
+
+  def parse_args(args) do
+    {options, _, _} = OptionParser.parse(args, switches: [out: :string])
+    options
+  end
 end
